1a0e34b4b5480802f49c094940378a04494a51ac
akshhack <akshatp@andrew.cmu.edu>
2017-06-11 21:27:35 -0400

    Changed OWNERSHIP to OWNERSHP

--- HomeTask1ProfLaurence/computingStatisticsForHouseholds.R
+++ HomeTask1ProfLaurence/computingStatisticsForHouseholds.R
@@ -18,7 +18,7 @@ absolute_directory <- "C:\\Users\\akshatp\\Desktop\\DataScienceInEconomics\\Home
 home_df <- read.csv(absolute_directory)
 
 #STRUCTURE OF DATABASE (in .csv):
- # | YEAR | SERIAL | HHWT | METAREA | OWNERSHIP | RENTGRS | HHINCOME | VALUEH |
+ # | YEAR | SERIAL | HHWT | METAREA | OWNERSHP | RENTGRS | HHINCOME | VALUEH |
  # unit of measurement is a household
 
 #split dataframe into list of dataframes by year and name them
@@ -27,30 +27,32 @@ names(home_dfs_by_year) <- c("1990", "2000", "2010")
 
 #get data of years 1990, 2000, 2010 in a list with duplicates removed, removed na
 #and only households with strictly positive income (HHINCOME)
-home_dfs_by_year <- lapply(home_dfs_by_year, function (x) x[!duplicated(x) & 
-                                                            x$HHINCOME > 0 & 
-                                                            complete.cases(x),])
+home_dfs_by_year <- lapply(home_dfs_by_year, function (df) df[!duplicated(df) & 
+                                                              df$HHINCOME > 0 & 
+                                                              complete.cases(df),])
 
 #HELPER FUNCTIONS TO REMOVE NULL DATA
 
 #remove households with VALUEHs as 9999999 or 9999998 or 0000000 as in NA
-remove_missing_VALUEHs <- function(x) {
-  subset(x, x$VALUEH != 9999999 & x$VALUEH != 9999998 & x$VALUEH != 0000000)
+remove_missing_VALUEHs <- function(df) {
+  subset(df, df$VALUEH != 9999999 & 
+             df$VALUEH != 9999998 & 
+             df$VALUEH != 0000000)
 }
 
 #remove housholds with OWNERSHIP as 0 as in NA
-remove_missing_OWNERSHIP <- function(x) {
-  subset(x, x$OWNERSHIP != 0)
+remove_missing_OWNERSHIP <- function(df) {
+  subset(df, df$OWNERSHP != 0)
 }
 
 #MASTER FUNCTION TO CALCULATE 10th, 90th PERCENTILES AND VARIANCE
-tenth_ninetieth_percentile_by_year_by_msas <- function(year, value = "VALUEH", ownership = 1, type = "percentile") {
-  year_by_msas <- split(year, year$METAREA)
+tenth_ninetieth_percentile_variance_by_year_by_msas <- function (year_df, value = "VALUEH", ownership = 1, type = "percentile") {
+  year_by_msas <- split(year_df, year_df$METAREA) 
   if (type == "percentile") {
-    lapply(year_by_msas, function(msa) quantile(subset(msa, msa$OWNERSHIP == ownership)[[value]], 
+    lapply(year_by_msas, function(msa) quantile(subset(msa, msa$OWNERSHP == ownership)[[value]], 
                                                 probs = c(0.10,0.90)))
   } else if (type == "variance"){
-    lapply(year_by_msas, function(msa) var(subset(msa, msa$OWNERSHIP == ownership)[[value]]))
+    lapply(year_by_msas, function(msa) var(subset(msa, msa$OWNERSHP == ownership)[[value]]))
   }
 } 
 
@@ -82,47 +84,47 @@ home_dfs_values_cleaned <- lapply(home_dfs_ownership_corrected, remove_missing_V
 #compute 10th, 90th percentile and variance of VALUEH for households that own main dwelling
 #for each year and each msa
 percentile_10th_90th_VALUEH_own_dwelling <- lapply(home_dfs_values_cleaned, 
-                                                   tenth_ninetieth_percentile_by_year_by_msas, 
+                                                   tenth_ninetieth_percentile_variance_by_year_by_msas, 
                                                    value = "VALUEH", ownership = 1,
                                                    type="percentile")
 variance_VALUEH_own_dwelling <- lapply(home_dfs_values_cleaned, 
-                                       tenth_ninetieth_percentile_by_year_by_msas, 
+                                       tenth_ninetieth_percentile_variance_by_year_by_msas, 
                                        value = "VALUEH", ownership = 1,
                                        type="variance")
 
 #compute 10th, 90th percentile and variance of RENTGRS for households that do not own main dwelling 
 #for each year and each msa
 percentile_10th_90th_RENTGRS_not_own_dwelling <- lapply(home_dfs_values_cleaned, 
-                                                        tenth_ninetieth_percentile_by_year_by_msas, 
+                                                        tenth_ninetieth_percentile_variance_by_year_by_msas, 
                                                         value = "RENTGRS", ownership = 2,
                                                         type="percentile")
 
 variance_RENTGRS_not_own_dwelling <- lapply(home_dfs_values_cleaned, 
-                                            tenth_ninetieth_percentile_by_year_by_msas, 
+                                            tenth_ninetieth_percentile_variance_by_year_by_msas, 
                                             value = "RENTGRS", ownership = 2,
                                             type="variance")
 
 #compute 10th, 90th percentile and variance of HHINCOME for households that own main dwelling
 #for each year and each msa
 percentile_10th_90th_HHINCOME_own_dwelling <- lapply(home_dfs_ownership_corrected, 
-                                                     tenth_ninetieth_percentile_by_year_by_msas, 
+                                                     tenth_ninetieth_percentile_variance_by_year_by_msas, 
                                                      value = "HHINCOME", ownership = 1,
                                                      type="percentile")
 
 variance_HHINCOME_own_dwelling <- lapply(home_dfs_ownership_corrected, 
-                                         tenth_ninetieth_percentile_by_year_by_msas, 
+                                         tenth_ninetieth_percentile_variance_by_year_by_msas, 
                                          value = "HHINCOME", ownership = 1,
                                          type="variance")
 
 #compute 10th, 90th percentile and variance of HHINCOME for households that own do not own main dwelling
 #for each year and each msa
 percentile_10th_90th_HHINCOME_not_own_dwelling <- lapply(home_dfs_ownership_corrected, 
-                                                         tenth_ninetieth_percentile_by_year_by_msas, 
+                                                         tenth_ninetieth_percentile_variance_by_year_by_msas, 
                                                          value = "HHINCOME", ownership = 2,
                                                          type="percentile")
 
 variance_HHINCOME_not_own_dwelling <- lapply(home_dfs_ownership_corrected, 
-                                             tenth_ninetieth_percentile_by_year_by_msas, 
+                                             tenth_ninetieth_percentile_variance_by_year_by_msas, 
                                              value = "HHINCOME", ownership = 2,
                                              type="variance")
 
